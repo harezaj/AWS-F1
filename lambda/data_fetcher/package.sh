e41d214bb996ac88b52b52591f5ce80058933ada
Jake Hareza <hareza.jake@gmail.com>
2025-03-29 19:43:07 -0400

    Reorganize directory structure and fix package.sh script

--- lambda/data_fetcher/package.sh
+++ lambda/data_fetcher/package.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-cd f1_data_fetcher
+# Install dependencies
 pip install -r requirements.txt -t ./package
+
+# Copy Lambda function code to package
 cp lambda_function.py ./package/
+
+# Create zip file
 cd package
 zip -r ../function.zip .
 cd ..
+
+# Clean up
 rm -rf package 
